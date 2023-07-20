d2cc83f0ffd2879a23b82b8a31f8d57f3c26fa40
Dale Bezolli <deilbezolli@gmail.com>
2023-07-20 11:15:00 +0000

    Add unit test for priceState formatter

--- webapp/test/unit/model/formatter.js
+++ webapp/test/unit/model/formatter.js
@@ -34,4 +34,35 @@ sap.ui.define([
 	QUnit.test("Should round a zero", function (assert) {
 		numberUnitValueTestCase.call(this, assert, "0", "0.00");
 	});
+
+	QUnit.module("Price State");
+	function priceStateTestCase(options) {
+		const state = formatter.priceState(options.price);
+		options.assert.strictEqual(state, options.expected, "The price state was correct");
+	}
+
+	QUnit.test("Should format the product with a price lower than 50 Success", function(assert) {
+		priceStateTestCase.call(this, { assert, price: 42, expected: "Success" });
+	});
+
+	QUnit.test("Should format the product with a price of 50 Normal", function(assert) {
+		priceStateTestCase.call(this, { assert, price: 50, expected: "None" });
+	});
+
+	QUnit.test("Should format the product with a price between 50 and 250 Normal", function(assert) {
+		priceStateTestCase.call(this, { assert, price: 249, expected: "None" });
+	});
+
+	QUnit.test("Should format the product with a price of 250 Warning", function(assert) {
+		priceStateTestCase.call(this, { assert, price: 250, expected: "Warning" });
+	});
+
+	QUnit.test("Should format the product with a price between 250 and 2000 Warning", function(assert) {
+		priceStateTestCase.call(this, { assert, price: 1999, expected: "Warning" });
+	});
+
+	QUnit.test("Should format the product with a price of 2000 and over Error", function(assert) {
+		priceStateTestCase.call(this, { assert, price: 2000, expected: "Error" });
+	});
+
 });
